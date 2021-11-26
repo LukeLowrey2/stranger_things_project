395cf8229ccf21f828d543874c29f047fcbffc22
LukeLowrey2 <89051599+LukeLowrey2@users.noreply.github.com>
2021-11-25 17:41:49 -0800

    Finished the Login and Password  component

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,23 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
+const BASE_URL = 'https://strangers-things.herokuapp.com/api/2008-USD-RM-WEB-PT/users/register';
+
 const LoginForm = () => {
-    
-    
-    return(
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                user: {
+                    username: username,
+                    password: password,
+                }
+            })
+        });
+
+        const {data : {token}} = await response.json();
+
+        localStorage.setItem('token', token);
+
+        setUsername('');
+        setPassword('');
+    }
+
+    const locallySourcedToken = localStorage.getItem('token');
+
+    return (
         <div>
-            <form>
-                <input type='text'>
+            <form onSubmit= {handleSubmit}>
+                <input type='text' value={username} onChange={(event) => setUsername(event.target.value)}>
                 </input>
 
-                <input type='text'>
+                <input type='text' value={password} onChange={(event) => setPassword(event.target.value)}>
                 </input>
 
                 <button type='submit'>
                 </button>
             </form>
+
+            <div>
+                {
+                    locallySourcedToken && locallySourcedToken.length ? <div> Logged In </div> : ''
+                }
+            </div>
         </div>
     )
+
+    
+
 }
 
 export default LoginForm;
